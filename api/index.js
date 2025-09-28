1fccc761ca5132009a410632e7912c4b9723a2c6
José Gabriel <jg.barros.dsantos@gmail.com>
2025-09-28 11:16:39 -0300

    atualização

--- api/index.js
+++ api/index.js
@@ -1,14 +1,12 @@
 // api/index.js
-import 'dotenv/config';
-import cors from 'cors';
 import express from 'express';
-import serverless from 'serverless-http'; // npm i serverless-http
+import cors from 'cors';
+import serverless from 'serverless-http';
 
 import models, { sequelize } from './models';
 import routes from './routes';
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,10 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/users', routes.user);
 app.use('/messages', routes.message);
 
-const eraseDatabaseOnSync = process.env.ERASE_DATABASE === 'true';
-sequelize.sync({ force: eraseDatabaseOnSync }).then(() => {
-  console.log('DB sync done.');
-});
+sequelize.sync().then(() => console.log("DB sincronizado."));
 
-// exporta o handler serverless — isso é o que a Vercel espera
 export default serverless(app);
